d95c631af36af88f88156298c1d9c0489421e831
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-03-24 14:12:32 +0100

    add formatTime & startTimer

--- app/script.js
+++ app/script.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+const formatTime = time => {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  return (
+    (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds <10 ? '0' + seconds : seconds)
+  );
+};
+
+
 class App extends React.Component {
   constructor() {
     super()
@@ -12,8 +21,17 @@ class App extends React.Component {
     };
   }
 
+  step = () => {};
+
+  startTimer = () => {
+    this.setState({
+      timer: setInterval(this.step, 1200),
+      status: 'work'
+    });
+  };
+
   render() {
-    const {status} = this.state;
+    const {status, time, timer} = this.state;
 
     return (
       <div>
@@ -26,8 +44,8 @@ class App extends React.Component {
         )}
         {(status === 'work') && <img src="./images/work.png" />}
         {(status === 'rest') && <img src="./images/rest.png" />}
-        {(status !== 'off') && <div className="timer">18:23</div>}
-        {(status === 'off') && <button className="btn">Start</button>}
+        {(status !== 'off') && <div className="timer">{formatTime(time)}</div>}
+        {(status === 'off') && <button className="btn" onClick={this.startTimer}>Start</button>}
         {(status !== 'off') && <button className="btn">Stop</button>}
         <button className="btn btn-close">X</button>
       </div>
